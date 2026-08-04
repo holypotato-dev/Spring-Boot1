bc11b6073ee15cdd0d55a9411c1a8895de5deb21
Aadil <itsmeaadil143@gmail.com>
2026-08-04 16:44:11 +0000

    Added @Repository in Repository

--- src/main/java/com/aadil/jobtracker/UserRepository/UserRepository.java
+++ src/main/java/com/aadil/jobtracker/UserRepository/UserRepository.java
@@ -6,7 +6,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-
+import java.util.Optional;
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
 
     boolean existsByName (String name);
